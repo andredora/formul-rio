2632c69d17b67329b775ac814ce2d5cb4df5dd22
andredora <117547102+andredora@users.noreply.github.com>
2022-11-08 16:21:29 +0000

    Create java script.js

---
+++ java script.js
@@ -0,0 +1,20 @@
+<!-- Bootstrap Bundle -->
+<script src="../lib/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+    (function () {
+        'use strict'
+        var forms = document.querySelectorAll('.needs-validation')
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+                    form.classList.add('was-validated')
+                }, false)
+            })
+    })()
+</script>
+</body>
+</html>
